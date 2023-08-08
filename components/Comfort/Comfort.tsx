83b6327254f44e21d66b0ca0178922af7e715ee1
Izatis <mekinovizat1@gmail.com>
2023-08-08 20:17:23 +0600

    okey

--- components/Comfort/Comfort.tsx
+++ components/Comfort/Comfort.tsx
@@ -1,54 +1,58 @@
 import Image from "next/image";
-import Phone from '../../assets/Phone.png'
-import AppStore from '../../assets/AppStore.png'
-import GooglePlay from '../../assets/GooglePlay.png'
-import s from './Comfort.module.scss'
+import Phone from "../../assets/Phone.png";
+import AppStore from "../../assets/AppStore.png";
+import GooglePlay from "../../assets/GooglePlay.png";
+import s from "./Comfort.module.scss";
 
 const Comfort = () => {
-    return (
-        <section className={s.comfort}>
-            <div className="container">
-                <div className="flex justify-between items-center">
-                    <div className="w-1/3">
-                        <h2 className="text-gray-900 text-6xl font-bold leading-10 mb-11"><span className="text-blue-400">Со</span> смартфона удобнее</h2>
+  return (
+    <section className={s.comfort}>
+      <div className="container">
+        <div className="flex justify-between items-center">
+          <div className="w-1/3">
+            <h2 className="text-gray-900 text-6xl font-bold leading-10 mb-11">
+              <span className="text-blue-400">Со</span> смартфона удобнее
+            </h2>
 
-                        <p className="text-gray-900 text-xl font-normal leading-8 mb-7">Ваши заказы и поддержка всегда под рукой</p>
+            <p className="text-gray-900 text-xl font-normal leading-8 mb-7">
+              Ваши заказы и поддержка всегда под рукой
+            </p>
 
-                        <ul className="flex flex-col gap-2 mb-7">
-                            <li className="flex items-center gap-3 text-gray-700 text-base font-normal leading-8">
-                                <span className={s.circle}></span>
-                                Ваши заказы и поддержка всегда под рукой
-                            </li>
+            <ul className="flex flex-col gap-2 mb-7">
+              <li className="flex items-center gap-3 text-gray-700 text-base font-normal leading-8">
+                <span className={s.circle}></span>
+                Ваши заказы и поддержка всегда под рукой
+              </li>
 
-                            <li className="flex items-center gap-3 text-gray-700 text-base font-normal leading-8">
-                                <span className={s.circle}></span>
-                                Ваши заказы и поддержка всегда под рукой
-                            </li>
+              <li className="flex items-center gap-3 text-gray-700 text-base font-normal leading-8">
+                <span className={s.circle}></span>
+                Ваши заказы и поддержка всегда под рукой
+              </li>
 
-                            <li className="flex items-center gap-3 text-gray-700 text-base font-normal leading-8">
-                                <span className={s.circle}></span>
-                                Ваши заказы и поддержка всегда под рукой
-                            </li>
-                        </ul>
+              <li className="flex items-center gap-3 text-gray-700 text-base font-normal leading-8">
+                <span className={s.circle}></span>
+                Ваши заказы и поддержка всегда под рукой
+              </li>
+            </ul>
 
-                        <div className="flex gap-5">
-                            <div className="w-52">
-                                <Image src={GooglePlay}/>
-                            </div>
+            <div className="flex gap-5">
+              <div className="w-52">
+                <Image src={GooglePlay} alt="GooglePlay" />
+              </div>
 
-                            <div className="w-48">
-                                <Image src={AppStore}/>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div className={`${s.phone}`}>
-                        <Image className="w-full" src={Phone}/>
-                    </div>
-                </div>
+              <div className="w-48">
+                <Image src={AppStore} alt="AppStore" />
+              </div>
             </div>
-        </section>
-    );
+          </div>
+
+          <div className={`${s.phone}`}>
+            <Image className="w-full" src={Phone} alt="Phone" />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Comfort;
