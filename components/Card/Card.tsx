22ac33fb9fcd82e668a728bd9cd556949520cf8d
Izatis <mekinovizat1@gmail.com>
2023-08-08 20:13:08 +0600

    okey

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, { FC } from "react";
+
 import Image from "next/image";
 
-const Card = ({item}) => {
-    return (
-        <div className="w-1/4">
-            <div>
-                <Image src={item.img}/>
-            </div>
+interface ICardPorps {
+  item: any;
+}
 
-            <div className="flex flex-col mt-3">
-                <p className="text-black text-2xl font-semibold">{item.title}</p>
-                <p className="text-gray text-base font-normal tracking-wider">от {item.price}₽</p>
-            </div>
-        </div>
-    );
+const Card: FC<ICardPorps> = ({ item }) => {
+  return (
+    <div className="w-1/4">
+      <div>
+        <Image src={item.img} alt="card image" />
+      </div>
+
+      <div className="flex flex-col mt-3">
+        <p className="text-black text-2xl font-semibold">{item.title}</p>
+        <p className="text-gray text-base font-normal tracking-wider">
+          от {item.price}₽
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
