43418107b15ec5ec5c70119deb1110f7343f691b
Izatis <mekinovizat1@gmail.com>
2023-08-08 15:12:45 +0600

    adding: hero

--- components/Direction/Direction.tsx
+++ components/Direction/Direction.tsx
@@ -1,29 +1,39 @@
+import s from "./Direction.module.scss";
+
 import Warning from "@/components/Warning/Warning";
 import Image from "next/image";
-import Sydney from '../../assets/Sydney.png'
+import Sydney from "../../assets/Sydney.png";
 
 const Direction = () => {
-    return (
-        <section className="pt-5">
-            <div className="container">
-                <Warning/>
+  return (
+    <main>
+      <section className={`${s.hero} mb-16`}>
+        <div className={s.container}>
+          <h1 className="text-8xl font-extrabold">
+            Путешествуй <br /> без ограничений
+          </h1>
+          <div className={`${s.tabs} rounded-2xl`}></div>
+        </div>
+      </section>
 
+      <div className="container pt-5">
+        <Warning />
 
-                <h2>Популярные направления</h2>
+        <h2>Популярные направления</h2>
 
-                <div>
-                    <div>
-                        <Image src={Sydney}/>
+        <div>
+          <div>
+            <Image src={Sydney} alt="sydney" />
 
-                        <div>
-                            <p>Сидней</p>
-                            <p>от 23509₽</p>
-                        </div>
-                    </div>
-                </div>
+            <div>
+              <p>Сидней</p>
+              <p>от 23509₽</p>
             </div>
-        </section>
-    );
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Direction;
